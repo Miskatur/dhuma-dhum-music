671675b36add7bfe47d1770ab6d97213c21d445d
Miskatur <Miskaturrahman34826@gmail.com>
2022-09-14 23:18:04 +0600

    debugging done

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const elementById = (id) => {
-  document.getElementById(id);
-};
-
 const handleSearch = () => {
-  const keyword = elementById("keyword");
+  const keyword = document.getElementById("keyword");
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
@@ -11,7 +7,8 @@ const handleSearch = () => {
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  const artistContainer = document.getElementById("artists");
+  artistContainer.textContent = '';
   data?.artists?.forEach((artist) => {
     const div = document.createElement("div");
     div.classList.add("artist-card");
@@ -37,31 +34,39 @@ const showArtists = (data) => {
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
-    .then((data) => showAlbum(data));
-  const artistContainer = elementById("artists");
+    .then(res => res.json())
+    .then(data => showAlbum(data));
+  const artistContainer = document.getElementById("artists");
   artistContainer.innerHTML = "";
+
 };
 
+
 const showAlbum = (data) => {
-  const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  const getArray = data.album;
+  const albumContainer = document.getElementById("albums");
+  albumContainer.textContent = '';
+  getArray.forEach(item => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${item.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
     albumContainer.appendChild(div);
   });
 };
+
+
+
+handleSearch('a')
